d313f4d7e8592ec353f8e8e6e9de2ab26aeccef4
ayapcck <ayapcck@gmail.com>
2019-11-18 19:21:24 -0800

    started mock data

--- src/ca/ubc/cs304/database/tables/Branch.sql
+++ src/ca/ubc/cs304/database/tables/Branch.sql
@@ -3,3 +3,9 @@ CREATE TABLE Branch(
     city VARCHAR(255),
     PRIMARY KEY (location, city)
 );
+
+INSERT INTO Branch (location, city) VALUES ('shop_1', 'Vancouver');
+INSERT INTO Branch (location, city) VALUES ('shop_2', 'Vancouver');
+INSERT INTO Branch (location, city) VALUES ('shop_3', 'Vancouver');
+INSERT INTO Branch (location, city) VALUES ('shop_1', 'Burnaby');
+INSERT INTO Branch (location, city) VALUES ('shop_2', 'Burnaby');
--- src/ca/ubc/cs304/database/tables/Customer.sql
+++ src/ca/ubc/cs304/database/tables/Customer.sql
@@ -5,3 +5,14 @@ CREATE TABLE Customer(
     driversLicense VARCHAR(255),
     PRIMARY KEY (cellNum)
 );
+
+INSERT INTO Customer (cellNum, name, address, driversLicense)
+    VALUES (6041234567, 'John Smith', '1234 made up lane', '9282019');
+INSERT INTO Customer (cellNum, name, address, driversLicense)
+    VALUES (1234567890, 'Cane Able', '4312 does not exist', 'license_num');
+INSERT INTO Customer (cellNum, name, address, driversLicense)
+    VALUES (0109238881, 'Smith Apple', 'Disney Land Resort', 'q1238822');
+INSERT INTO Customer (cellNum, name, address, driversLicense)
+    VALUES (8388772838, 'Jonny Rocket', 'Tim Hortons', '920019293');
+INSERT INTO Customer (cellNum, name, address, driversLicense)
+    VALUES (8289199482, 'Robert Frost', '9829 something road', '9maybe8912');
