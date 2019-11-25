dac77114ef5f725e97383c2f5afdf1612ae036b7
Tarandeep Natt <tarandeep1598@gmail.com>
2019-11-24 20:07:07 -0800

    approve this (#28)

--- src/ca/ubc/cs304/database/AddTablesAndData.sql
+++ src/ca/ubc/cs304/database/AddTablesAndData.sql
@@ -1,119 +1,119 @@
 CREATE TABLE TimePeriod(
-    fromDate VARCHAR(255),
-    fromTime VARCHAR(255),
-    toDate VARCHAR(255),
-    toTime VARCHAR(255),
-    PRIMARY KEY (fromDate, fromTime, toDate, toTime)
+                           fromDate VARCHAR(255),
+                           fromTime VARCHAR(255),
+                           toDate VARCHAR(255),
+                           toTime VARCHAR(255),
+                           PRIMARY KEY (fromDate, fromTime, toDate, toTime)
 );
 
 CREATE TABLE Customer(
-    cellNum VARCHAR(255),
-    name VARCHAR(255),
-    address VARCHAR(255),
-    driversLicense VARCHAR(255),
-    PRIMARY KEY (driversLicense)
+                         cellNum VARCHAR(255),
+                         name VARCHAR(255),
+                         address VARCHAR(255),
+                         driversLicense VARCHAR(255),
+                         PRIMARY KEY (driversLicense)
 );
 
 CREATE TABLE Branch(
-    location VARCHAR(255),
-    city VARCHAR(255),
-    PRIMARY KEY (location, city)
+                       location VARCHAR(255),
+                       city VARCHAR(255),
+                       PRIMARY KEY (location, city)
 );
 
 CREATE TABLE VehicleType(
-    VTName VARCHAR(255),
-    features VARCHAR(255),
-    weeklyRate INTEGER DEFAULT 1000,
-    dailyRate INTEGER DEFAULT 100,
-    hourlyRate INTEGER DEFAULT 10,
-    weeklyInsuranceRate INTEGER DEFAULT 1000,
-    dailyInsuranceRate INTEGER DEFAULT 100,
-    hourlyInsuranceRate INTEGER DEFAULT 10,
-    kmRate INTEGER DEFAULT 2,
-    PRIMARY KEY (VTName)
+                            VTName VARCHAR(255),
+                            features VARCHAR(255),
+                            weeklyRate INTEGER DEFAULT 1000,
+                            dailyRate INTEGER DEFAULT 100,
+                            hourlyRate INTEGER DEFAULT 10,
+                            weeklyInsuranceRate INTEGER DEFAULT 1000,
+                            dailyInsuranceRate INTEGER DEFAULT 100,
+                            hourlyInsuranceRate INTEGER DEFAULT 10,
+                            kmRate INTEGER DEFAULT 2,
+                            PRIMARY KEY (VTName)
 );
 
 CREATE TABLE ForRent(
-    vID INTEGER,
-    vLicense VARCHAR(255),
-    make VARCHAR(255) DEFAULT 'Volkswagen',
-    model VARCHAR(255) DEFAULT 'Beetle',
-    year INTEGER DEFAULT 2000,
-    color VARCHAR(255),
-    odometer INTEGER DEFAULT 0,
-    status VARCHAR(255),
-    VTName VARCHAR(255) NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    PRIMARY KEY (vLicense),
-    FOREIGN KEY (location, city) REFERENCES Branch,
-    FOREIGN KEY (VTName) REFERENCES VehicleType,
-    CHECK (status = 'available' OR status = 'notavailable')
+                        vID INTEGER,
+                        vLicense VARCHAR(255),
+                        make VARCHAR(255) DEFAULT 'Volkswagen',
+                        model VARCHAR(255) DEFAULT 'Beetle',
+                        year INTEGER DEFAULT 2000,
+                        color VARCHAR(255),
+                        odometer INTEGER DEFAULT 0,
+                        status VARCHAR(255),
+                        VTName VARCHAR(255) NOT NULL,
+                        location VARCHAR(255) NOT NULL,
+                        city VARCHAR(255) NOT NULL,
+                        PRIMARY KEY (vLicense),
+                        FOREIGN KEY (location, city) REFERENCES Branch,
+                        FOREIGN KEY (VTName) REFERENCES VehicleType,
+                        CHECK (status = 'available' OR status = 'notavailable')
 );
 
 CREATE TABLE ForSale(
-    vID      INTEGER,
-    vFSLicense VARCHAR(255),
-    make     VARCHAR(255),
-    model    VARCHAR(255),
-    year     INTEGER,
-    color    VARCHAR(255),
-    odometer INTEGER,
-    status   VARCHAR(255),
-    VTName   VARCHAR(255) NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    city     VARCHAR(255) NOT NULL,
-    PRIMARY KEY (vFSLicense),
-    FOREIGN KEY (location, city) REFERENCES Branch,
-    FOREIGN KEY (VTName) REFERENCES VehicleType,
-    CHECK (status = 'available' OR status = 'notavailable')
+                        vID      INTEGER,
+                        vFSLicense VARCHAR(255),
+                        make     VARCHAR(255),
+                        model    VARCHAR(255),
+                        year     INTEGER,
+                        color    VARCHAR(255),
+                        odometer INTEGER,
+                        status   VARCHAR(255),
+                        VTName   VARCHAR(255) NOT NULL,
+                        location VARCHAR(255) NOT NULL,
+                        city     VARCHAR(255) NOT NULL,
+                        PRIMARY KEY (vFSLicense),
+                        FOREIGN KEY (location, city) REFERENCES Branch,
+                        FOREIGN KEY (VTName) REFERENCES VehicleType,
+                        CHECK (status = 'available' OR status = 'notavailable')
 );
 
 CREATE TABLE Reservations(
-    confNo INTEGER,
-    VTName VARCHAR(255) NOT NULL,
-    driversLicense VARCHAR(255) NOT NULL,
-    fromDate VARCHAR(255),
-    fromTime VARCHAR(255),
-    toDate VARCHAR(255),
-    toTime VARCHAR(255),
-    location VARCHAR(255) NOT NULL,
-    city     VARCHAR(255) NOT NULL,
-    PRIMARY KEY (confNo),
-    FOREIGN KEY (VTName) REFERENCES VehicleType,
-    FOREIGN KEY (location, city) REFERENCES Branch,
-    FOREIGN KEY (driversLicense) REFERENCES Customer,
-    FOREIGN KEY (fromDate, fromTime, toDate, toTime) REFERENCES TimePeriod
+                             confNo INTEGER,
+                             VTName VARCHAR(255) NOT NULL,
+                             driversLicense VARCHAR(255) NOT NULL,
+                             fromDate VARCHAR(255),
+                             fromTime VARCHAR(255),
+                             toDate VARCHAR(255),
+                             toTime VARCHAR(255),
+                             location VARCHAR(255) NOT NULL,
+                             city     VARCHAR(255) NOT NULL,
+                             PRIMARY KEY (confNo),
+                             FOREIGN KEY (VTName) REFERENCES VehicleType,
+                             FOREIGN KEY (location, city) REFERENCES Branch,
+                             FOREIGN KEY (driversLicense) REFERENCES Customer,
+                             FOREIGN KEY (fromDate, fromTime, toDate, toTime) REFERENCES TimePeriod
 );
 
 CREATE TABLE Rental(
-    rID INTEGER,
-    vLicense VARCHAR(255) NOT NULL,
-    driversLicense VARCHAR(255) NOT NULL,
-    fromDate VARCHAR(255) NOT NULL,
-    fromTime VARCHAR(255) NOT NULL,
-    toDate VARCHAR(255) NOT NULL,
-    toTime VARCHAR(255) NOT NULL,
-    odometer INTEGER,
-    cardName VARCHAR(255),
-    cardNo INTEGER,
-    expDate VARCHAR(255),
-    confNo INTEGER,
-    PRIMARY KEY (rID),
-    FOREIGN KEY (vLicense) REFERENCES ForRent,
-    FOREIGN KEY (driversLicense) REFERENCES Customer,
-    FOREIGN KEY (fromDate, fromTime, toDate, toTime) REFERENCES TimePeriod,
-    FOREIGN KEY (confNo) REFERENCES Reservations
+                       rID INTEGER,
+                       vLicense VARCHAR(255) NOT NULL,
+                       driversLicense VARCHAR(255) NOT NULL,
+                       fromDate VARCHAR(255) NOT NULL,
+                       fromTime VARCHAR(255) NOT NULL,
+                       toDate VARCHAR(255) NOT NULL,
+                       toTime VARCHAR(255) NOT NULL,
+                       odometer INTEGER,
+                       cardName VARCHAR(255),
+                       cardNo INTEGER,
+                       expDate VARCHAR(255),
+                       confNo INTEGER,
+                       PRIMARY KEY (rID),
+                       FOREIGN KEY (vLicense) REFERENCES ForRent,
+                       FOREIGN KEY (driversLicense) REFERENCES Customer,
+                       FOREIGN KEY (fromDate, fromTime, toDate, toTime) REFERENCES TimePeriod,
+                       FOREIGN KEY (confNo) REFERENCES Reservations
 );
 
 CREATE TABLE Return(
-    rID INTEGER,
-    returnDate DATE,
-    odometer INTEGER,
-    fullTank NUMBER(1),
-    value INTEGER,
-    PRIMARY KEY (rID),
-    FOREIGN KEY (rID) REFERENCES Rental
+                       rID INTEGER,
+                       returnDate DATE,
+                       odometer INTEGER,
+                       fullTank NUMBER(1),
+                       value INTEGER,
+                       PRIMARY KEY (rID),
+                       FOREIGN KEY (rID) REFERENCES Rental
 );
 
 INSERT INTO Branch (location, city) VALUES ('shop_1', 'Vancouver');
@@ -205,6 +205,41 @@ INSERT INTO TIMEPERIOD (fromDate, fromTime, toDate, toTime)
 VALUES ('2019-11-27', '12:05', '2019-01-20', '1:00');
 
 
+INSERT INTO Reservations (confNo, VTName, driversLicense, fromDate, fromTime, toDate, toTime, location, city)
+VALUES (123, 'truck', '9282019', '2019-11-24', '12:05', '2020-01-20', '1:00', 'shop_1', 'Vancouver');
+
+
+INSERT INTO Reservations (confNo, VTName, driversLicense, fromDate, fromTime, toDate, toTime, location, city)
+VALUES (124, 'truck', '9282019', '2019-11-20', '12:05', '2019-11-25', '1:00', 'shop_1', 'Vancouver');
+
+INSERT INTO Reservations (confNo, VTName, driversLicense, fromDate, fromTime, toDate, toTime, location, city)
+VALUES (125, 'truck', '9272019', '2019-11-24', '12:05', '2020-01-20', '1:00', 'shop_1', 'Vancouver');
+
+INSERT INTO Reservations (confNo, VTName, driversLicense, fromDate, fromTime, toDate, toTime, location, city)
+VALUES (126, 'full-size', '9262019', '2019-11-24', '12:05', '2020-01-20', '1:00', 'shop_1', 'Vancouver');
+
+INSERT INTO Reservations (confNo, VTName, driversLicense, fromDate, fromTime, toDate, toTime, location, city)
+VALUES (127, 'full-size', '9252019', '2019-11-24', '12:05', '2020-01-20', '1:00', 'shop_1', 'Vancouver');
+
+INSERT INTO Reservations (confNo, VTName, driversLicense, fromDate, fromTime, toDate, toTime, location, city)
+VALUES (128, 'economy', '9242019', '2019-11-24', '12:05', '2020-01-20', '1:00', 'shop_1', 'Vancouver');
+
+INSERT INTO Reservations (confNo, VTName, driversLicense, fromDate, fromTime, toDate, toTime, location, city)
+VALUES (129, 'economy', '9232019', '2019-11-24', '12:05', '2020-01-20', '1:00', 'shop_1', 'Vancouver');
+
+INSERT INTO Reservations (confNo, VTName, driversLicense, fromDate, fromTime, toDate, toTime, location, city)
+VALUES (130, 'economy', '9222019', '2019-11-24', '12:05', '2020-01-20', '1:00', 'shop_1', 'Vancouver');
+
+INSERT INTO Rental(rID ,vLicense, driversLicense , fromDate , fromTime, toDate, toTime, odometer,cardName,cardNo ,expDate, confNo)
+VALUES (125, 'A10115', '9282019', '2019-11-20', '12:05', '2019-11-25', '1:00', 0, 'VISA', 123456, '02/21', 124);
+
+INSERT INTO Rental(rID ,vLicense, driversLicense , fromDate , fromTime, toDate, toTime, odometer,cardName,cardNo ,expDate, confNo)
+VALUES (126, 'A10116', '9272019', '2019-11-24', '12:05', '2020-01-20', '1:00', 0, 'VISA', 123456, '02/21', 125);
+
+INSERT INTO Rental(rID ,vLicense, driversLicense , fromDate , fromTime, toDate, toTime, odometer,cardName,cardNo ,expDate, confNo)
+VALUES (127, 'A10112', '9262019', '2019-11-24', '12:05', '2020-01-20', '1:00', 0, 'VISA', 123456, '02/21', 126);
+
+
 INSERT INTO Rental(rID ,vLicense, driversLicense , fromDate , fromTime, toDate, toTime, odometer,cardName,cardNo ,expDate, confNo)
 VALUES (128, 'A10113', '9252019', '2019-11-24', '12:05', '2020-01-20', '1:00', 0, 'VISA', 123456, '02/21', 127);
 
@@ -220,7 +255,7 @@ INSERT INTO Rental(rID ,vLicense, driversLicense , fromDate , fromTime, toDate, 
 VALUES (131, 'A10119', '9222019', '2019-11-24', '12:05', '2020-01-20', '1:00', 0, 'VISA', 123456, '02/21', 130);
 
 INSERT INTO Return(rID, returnDate, odometer, fullTank, value)
-    VALUES(125, '2019-11-25', 5, 1, 510);
+VALUES(125, '2019-11-25', 5, 1, 510);
 
 INSERT INTO Return(rID, returnDate, odometer, fullTank, value)
 VALUES(126, '2020-01-20', 5, 1, 410);
