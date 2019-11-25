42e84f376c7ed83f70412d4c07880fb42b1e8d10
Tarandeep Natt <tarandeep1598@gmail.com>
2019-11-24 16:53:26 -0800

    Pull master taran (#21)
    * reports still not working
    
    * dont merge
    
    * dont merge
    
    * dont merge
    
    * merge this. Bug fixes

--- src/ca/ubc/cs304/ui/RentalWithoutReservationWindow.java
+++ src/ca/ubc/cs304/ui/RentalWithoutReservationWindow.java
@@ -94,11 +94,13 @@ public class RentalWithoutReservationWindow extends Window implements ActionList
             Date toDateTime = (Date) timeSpinnerTo.getValue();
             String fromDate = df.format(fromDateTime);
             String fromTime = tf.format(fromDateTime);
-            String toDate = df.format(fromDateTime);
+            String toDate = df.format(toDateTime);
             String toTime = tf.format(toDateTime);
             TimePeriod timePeriod = new TimePeriod(fromDate, fromTime, toDate, toTime);
 
-            Reservation reservation = new Reservation(null, vehicleTypeField.getText(),
+            int confNo = (int) (Math.random() * 100);
+            System.out.println(confNo);
+            Reservation reservation = new Reservation(confNo, vehicleTypeField.getText(),
                     licenseField.getText(), timePeriod);
             Branch branch = new Branch(locationField.getText(), cityField.getText());
             rentVehicleDelegate.rentWithoutReservation(reservation, branch,
