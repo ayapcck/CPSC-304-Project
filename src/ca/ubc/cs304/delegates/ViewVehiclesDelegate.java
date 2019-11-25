5238c3130e130971af75e379b85c93d3e492b9ab
monicaliu <liuwenyi@students.cs.ubc.ca>
2019-11-24 17:06:55 -0800

    view vehicle working

--- src/ca/ubc/cs304/delegates/ViewVehiclesDelegate.java
+++ src/ca/ubc/cs304/delegates/ViewVehiclesDelegate.java
@@ -1,6 +1,6 @@
 package ca.ubc.cs304.delegates;
 
 public interface ViewVehiclesDelegate {
-    void submit(String carType, String location, String city);
+    void submit(String carType, String location, String city, String fromDate, String toDate);
     void returnToCustomer();
 }
